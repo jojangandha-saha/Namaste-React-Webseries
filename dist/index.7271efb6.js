aae05fbd2f769d8e4941e072b771503e919e5b64
Jojangandha Saha <sj6007700@gmail.com>
2024-03-24 15:05:02 +0530

    updated cors url issue

--- dist/index.7271efb6.js
+++ dist/index.7271efb6.js
@@ -27905,8 +27905,7 @@ parcelHelpers.export(exports, "LOGO_URL", ()=>LOGO_URL);
 parcelHelpers.export(exports, "API_URL", ()=>API_URL);
 const CDN_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 const LOGO_URL = "https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018";
-const API_URL = // "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.4930383%26lng%3D88.36645080000001%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
-"https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4930383&lng=88.36645080000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+const API_URL = "https://food-villa-server.vercel.app/api/restaurants?lat=22.4930383&lng=88.36645080000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
 exports.interopDefault = function(a) {
--- utils/constants.jsx
+++ utils/constants.jsx
@@ -5,6 +5,9 @@ export const LOGO_URL =
   "https://images-platform.99static.com/A_Ax0GQuo_NHI0Y7XZHmFtGfBDY=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/126/126252/attachment_126252018";
 
 //single file multiple export - default exprot must be once - named export
+// export const API_URL =
+//   // "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.4930383%26lng%3D88.36645080000001%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
+//   "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4930383&lng=88.36645080000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+
 export const API_URL =
-  // "https://corsproxy.org/?https%3A%2F%2Fwww.swiggy.com%2Fdapi%2Frestaurants%2Flist%2Fv5%3Flat%3D22.4930383%26lng%3D88.36645080000001%26is-seo-homepage-enabled%3Dtrue%26page_type%3DDESKTOP_WEB_LISTING";
-  "https://thingproxy.freeboard.io/fetch/https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4930383&lng=88.36645080000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  "https://food-villa-server.vercel.app/api/restaurants?lat=22.4930383&lng=88.36645080000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
